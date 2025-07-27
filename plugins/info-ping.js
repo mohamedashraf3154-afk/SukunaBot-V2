438e0425046f2002b4ddf89104e143c0b77c4765
the-27 <blackoficial2025@gmail.com>
2025-07-27 11:11:07 -0500

    ＳＵＫＵＮＡ 🌳

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,43 +1,19 @@
-import { exec } from 'child_process'
 import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
 
-let handler = async (m, { conn, participants }) => {
-  let start = speed()
-  let latency = (speed() - start).toFixed(3)
+let handler = async (m, { conn }) => {
+         let timestamp = speed();
+         let latensi = speed() - timestamp;
+         exec(`neofetch --stdout`, (error, stdout, stderr) => {
+          let child = stdout.toString("utf-8");
+          let ssd = child.replace(/Memory:/, "Ram:");
 
-  exec('neofetch --stdout', (_, stdout) => {
-    let sys = stdout?.toString('utf-8').replace(/Memory:/g, 'Ram:') || ''
-    let info = sys.split('\n').map(line => `┃ ${line}`).join('\n')
-
-    let text = `
-╭─⭑✔️︎・*SUKUNA BOT MODE*・,🎄⭑─╮
-┃ 🧬 *Sistema activo:*  
-┃ ⚡ *Latencia:* ${latency} ms
-┃ 🛠️ *Detalles técnicos:*
-${info}
-╰━━━━━━━━━━━━━━━━━━━━⬣`.trim()
-
-    conn.sendMessage(m.chat, {
-      text,
-      mentions: participants?.map(p => p.id) || [],
-      contextInfo: {
-        mentionedJid: participants?.map(p => p.id) || [],
-        externalAdReply: {
-          title: packname,
-          body: dev,
-          thumbnailUrl: logo,
-          mediaType: 1,
-          showAdAttribution: true,
-          renderLargerThumbnail: true,
-          sourceUrl: 'https://github.com/the-27/Rin-Itoshi-Bot-V2'
-        }
-      }
-    }, { quoted: m })
-  })
+          conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms`, m, fake);
+            });
 }
-
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
+handler.register = true
 
 export default handler
