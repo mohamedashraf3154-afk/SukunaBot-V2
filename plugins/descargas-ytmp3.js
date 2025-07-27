65a76ccebf60219bc96c470a41027606f55833af
satoru-27 <blackoficial2025@gmail.com>
2025-07-27 16:46:59 -0500

    update

--- plugins/descargas-ytmp3.js
+++ plugins/descargas-ytmp3.js
@@ -45,7 +45,7 @@ const handler = async (m, { conn, text, command }) => {
     }, { quoted: m });
 
 
-    const api = `https://api.stellarwa.xyz/dow/ytmp3?url=${url}&apikey=estelar-ReKwdxiR`;
+    const api = `https://api.stellarwa.xyz/dow/ytmp3?url=${url}&apikey=stellar-ReKwdxiR`;
     const res = await fetch(api);
     const json = await res.json();
 
--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -43,7 +43,7 @@ const handler = async (m, { conn, text, usedPrefix, command, args }) => {
     const canal = author.name || 'Desconocido';
     const vistas = views.toLocaleString('es-PE');
 
-    const { data } = await axios.get(`https://api.stellarwa.xyz/dow/ytmp4?url=${encodeURIComponent(url)}&apikey=estelar-ReKwdxiR`);
+    const { data } = await axios.get(`https://api.stellarwa.xyz/dow/ytmp4?url=${encodeURIComponent(url)}&apikey=stellar-ReKwdxiR`);
     if (!data?.status || !data?.data?.dl) {
       throw new Error("No se pudo obtener el enlace de descarga.");
     }
